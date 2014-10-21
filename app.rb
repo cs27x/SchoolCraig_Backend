4d06b2d6f1e6ef5b5843c0a9b1f447cdf8541e26
Walton Seymour <bigw369@gmail.com>
2014-10-21 15:09:15 -0500

    added post routes for object creation

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@
 require 'sinatra'
 require 'json'
 require 'sinatra/activerecord'
+require 'securerandom'
 
 RACK_ENV = (ENV["RACK_ENV"] ||= "development").to_sym
 set :environment, RACK_ENV
-
 
 configure :production do
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
@@ -26,11 +26,17 @@ configure :development do
   )
 end
 
+########## Post Class #############
+# Used to model posts with ActiveRecord and
+# communicate with the Postgres DB
 class Post < ActiveRecord::Base
   self.table_name = 'posts'
   self.primary_key = 'id'
 end
 
+########## User Class #############
+# Used to model users with ActiveRecord and
+# communicate with the Postgres DB
 class User < ActiveRecord::Base
   self.table_name = 'users'
   self.primary_key = 'id'
@@ -61,5 +67,42 @@ get '/user/id/:id' do |id|
     User.find(id).to_json
   rescue ActiveRecord::RecordNotFound
     {}.to_json
+  end
+end
+
+post '/post/new/?' do
+  content_type :json
+  # parses body of post request
+  body = request.body.read
+  body = JSON.parse(body)
+  user = body['user']
+  description = body['description']
+
+  if !(description.nil? || user.nil?)
+    # logic for email verification goes here
+    uuid = SecureRandom.uuid
+    Post.create(id: uuid, user: user, description: description)
+    { status: 'success' }.to_json
+  else
+    { status: 'failure' }.to_json
+  end
+end
+
+post '/user/new/?' do
+  content_type :json
+  # parses body of post request
+  body = request.body.read
+  body = JSON.parse(body)
+  fname = body['fname']
+  lname = body['lname']
+  email = body['email']
+
+  if !(fname.nil? || lname.nil? || email.nil?)
+    # logic for email verification goes here
+    uuid = SecureRandom.uuid
+    User.create(id: uuid, fname: fname, lname: lname, email: email)
+    { status: 'success' }.to_json
+  else
+    { status: 'failure' }.to_json
   end
 end
