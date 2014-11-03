3bd3c5c047f2030a00de4a17842e68a9f53dab0e
monicahedda <monica.hedda@vanderbilt.edu>
2014-11-03 12:19:21 -0600

    fixed uuid, other fix still in progress

--- app_test.rb
+++ app_test.rb
@@ -11,6 +11,11 @@ class MyAppTest < MiniTest::Unit::TestCase
   def app
     Sinatra::Application
   end
+  
+  def setup
+      @@user_uuid = SecureRandom.uuid
+      @@post_uuid = SecureRandom.uuid
+  end
 
   ## Users
   
@@ -20,44 +25,32 @@ class MyAppTest < MiniTest::Unit::TestCase
   end
   
   def test_post_user
-    post '/user', body = {"id" => "1", "fname" => "Alex"}
-    assert last_response.ok? , "Error in posting a users"
+    post '/user', body = { "id" => @@user_uuid, "fname" => "Alex" , "lname" => "Alex", "email" => "test@vanderbilt.edu", "password" => "pwd" }
+    assert last_response.ok? , "Error in creating a users"
   end
   
-  #Not sure about IDs
-  def test_get_user_ok
-    get 'user/id',id: "1"
-    assert last_response.ok? , "Error in getting a user"
-  end 
-
   def test_put_user
-    put '/user/id', params = {"id" => "1", "name" => "Bob"}
+    put '/user/id', params = { "userid" => @@user_uuid }
     assert last_response.ok? , "Error in put user"
   end
 
-  def test_delete_user
-    delete 'user/id',id: "1" 
-    assert last_response.ok?, "Error in deleting a user"
-  end
-  
   ## User Authentication and Activation
-  # Have no idea how this works
   
   def test_post_user_auth
-    post '/user/auth', params = {"name" => "Alex"}
+    post '/user/auth', body = { "email" => "test@vanderbilt.edu", "password" => "pwd" }
     assert last_response.ok?, "Error in user auth"
   end
   
-  def test_post_user_deauth
-    post '/user/deauth'
-    assert last_response.ok?, "Error in user deauth"
-  end
-  
-  #def test_get_user_activate
-   #get 'user/activate/id', id: "1"
-    #assert last_response.ok?, "Error in user activate"
-  #end 
+  def test_get_user_activate
+   get 'user/activate/:id', id: @@user_uuid
+   assert last_response.ok?, "Error in user activate"
+  end 
 
+  def test_get_user_ok
+    get 'user/id', id: @@user_uuid
+    assert last_response.ok? , "Error in getting a user"
+  end 
+  
   ## Posts
   
   def test_get_all_posts_ok
@@ -66,25 +59,25 @@ class MyAppTest < MiniTest::Unit::TestCase
   end
   
   def test_post_post
-    post '/post/', params = {"name" => "titleofpost"} , body ={"id" => "1"}
+    post '/post/', body = { "id" => @@user_uuid, "description" => "New post" }
     assert last_response.ok?, "Error in creating a post"
   end
   
-  #Not sure about IDs
   def test_get_post_ok
-    get '/post/id', id: "1"
+    get '/post/id', id: @@post_uuid
     assert last_response.ok?, "Error in getting a post"
   end
 
   def test_put_post
-    put '/user/id/titleofpost', params = {"name" => "newposttitle"}
+    put '/post/id/postid', params = { "postid" => @@post_uuid }
     assert last_response.ok?, "Error in put post"
   end
 
   def test_delete_post
-    delete '/user/id/newposttitle'
+    delete '/post/id/postid', params = { "postid" => @@post_uuid }
     assert last_response.ok?, "Error in deleting a post"
   end
+
 
   ## Categories  
   
@@ -93,9 +86,20 @@ class MyAppTest < MiniTest::Unit::TestCase
     assert last_response.ok?, "Error in getting all categories"
   end
 
-  #Not sure about IDs
+  #Not sure about category_id
   def test_get_category_ok
-    get '/categories/id/electronics'
+    get '/categories/id', category_id: "electronics"
     assert last_response.ok?, "Error in getting a catogory"
   end       
+
+  def test_post_user_deauth
+      post '/user/deauth'
+      assert last_response.ok?, "Error in user deauth"
+  end
+
+  def test_delete_user
+      delete 'user/id', id:  @@user_uuid 
+      assert last_response.ok?, "Error in deleting a user"
+  end
+  
 end
