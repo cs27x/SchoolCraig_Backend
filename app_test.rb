11c199c9b44b99d9a354fa9c08b1959af5ec1b29
monicahedda <monica.hedda@vanderbilt.edu>
2014-11-03 00:33:08 -0600

    Added failure message and fixed ID errors

--- app_test.rb
+++ app_test.rb
@@ -14,35 +14,35 @@ class MyAppTest < MiniTest::Unit::TestCase
 
   def test_root
     get '/'
-    assert last_response.ok?
+    assert last_response.ok? , "Error in get"
   end
   
   ## Users
   
   def test_get_all_users_ok
     get '/user'
-    assert last_response.ok?
+    assert last_response.ok? , "Error in getting all users"
   end
   
   def test_post_user
-    post '/user/', params = {"name" => "Alex"}
-    assert last_response.ok?
+    post '/user/', params = {"name" => "Alex"}, body ={"id" => "1"}
+    assert last_response.ok? , "Error in posting a users"
   end
   
   #Not sure about IDs
   def test_get_user_ok
-    get '/user/id/Alex'
-    assert last_response.ok?
+    get 'user/id',id: "1"
+    assert last_response.ok? , "Error in getting a user"
   end 
 
   def test_put_user
-    put '/user/id/Alex', params = {"name" => "Bob"}
-    assert last_response.ok?
+    put '/user/id', params = {"id" => "1", "name" => "Bob"}
+    assert last_response.ok? , "Error in put user"
   end
 
   def test_delete_user
-    delete '/user/id/Bob'
-    assert last_response.ok?
+    delete 'user/id',id: "1" 
+    assert last_response.ok?, "Error in deleting a user"
   end
   
   ## User Authentication and Activation
@@ -50,57 +50,57 @@ class MyAppTest < MiniTest::Unit::TestCase
   
   def test_post_user_auth
     post '/user/auth', params = {"name" => "Alex"}
-    assert last_response.ok?
+    assert last_response.ok?, "Error in user auth"
   end
   
   def test_post_user_deauth
     post '/user/deauth'
-    assert last_response.ok?
+    assert last_response.ok?, "Error in user deauth"
   end
   
-  def test_get_user_activate
-    get '/user/activate/Alex'
-    assert last_response.ok?
-  end 
+  #def test_get_user_activate
+   #get 'user/activate/id', id: "1"
+    #assert last_response.ok?, "Error in user activate"
+  #end 
 
   ## Posts
   
   def test_get_all_posts_ok
     get '/post'
-    assert last_response.ok?
+    assert last_response.ok?, "Error in getting all posts"
   end
   
   def test_post_post
-    post '/post/', params = {"name" => "titleofpost"}
-    assert last_response.ok?
+    post '/post/', params = {"name" => "titleofpost"} , body ={"id" => "1"}
+    assert last_response.ok?, "Error in creating a post"
   end
   
   #Not sure about IDs
   def test_get_post_ok
-    get '/post/id/titleofpost'
-    assert last_response.ok?
+    get '/post/id', id: "1"
+    assert last_response.ok?, "Error in getting a post"
   end
 
   def test_put_post
     put '/user/id/titleofpost', params = {"name" => "newposttitle"}
-    assert last_response.ok?
+    assert last_response.ok?, "Error in put post"
   end
 
   def test_delete_post
     delete '/user/id/newposttitle'
-    assert last_response.ok?
+    assert last_response.ok?, "Error in deleting a post"
   end
 
   ## Categories  
   
   def test_get_all_categories_ok
     get '/categories/all'
-    assert last_response.ok?
+    assert last_response.ok?, "Error in getting all categories"
   end
 
   #Not sure about IDs
   def test_get_category_ok
     get '/categories/id/electronics'
-    assert last_response.ok?
+    assert last_response.ok?, "Error in getting a catogory"
   end       
 end
